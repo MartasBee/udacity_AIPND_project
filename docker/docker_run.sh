3be0196b4cf1b0abcff17a42935fc0cb3651c8c5
MartasBee <draconis.sigma@gmail.com>
2021-07-14 21:00:44 +0200

    neccessary files to start docker image

---
+++ docker/docker_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#xhost +
+xhost +si:localuser:$USER
+
+docker run --gpus '"device=1","capabilities=compute,graphics,display,utility"' -it --rm \
+  -p 6006:6006 \
+  -p 8888:8888 \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /home/martin/devel/github/martasbee/udacity_AIPND_project:/tmp/repo \
+  -v /home/martin/devel/data:/tmp/data \
+  nvcr.io/nvidia/pytorch:21.05-py3 bash
