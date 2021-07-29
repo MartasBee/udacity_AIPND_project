07024d225946587c55739fae0edd43c9dc469597
MartasBee <draconis.sigma@gmail.com>
2021-07-29 21:45:15 +0200

    changed version of container

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -3,11 +3,11 @@
 #xhost +
 xhost +si:localuser:$USER
 
-docker run --gpus '"device=0","capabilities=compute,graphics,display,utility"' -it --rm \
+docker run --gpus '"device=1","capabilities=compute,graphics,display,utility"' -it --rm \
   -p 6006:6006 \
   -p 8888:8888 \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /home/martin/devel/github/martasbee/udacity_AIPND_project:/workspace/AIPND/repo \
   -v /home/martin/devel/data:/workspace/AIPND/data \
-  nvcr.io/nvidia/pytorch:21.07-py3 bash
+  nvcr.io/nvidia/pytorch:21.05-py3 bash
